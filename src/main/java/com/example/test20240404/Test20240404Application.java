c9e619bb46373584830f8089f048b6e8d2aaf2f8
xstahol <staffan.holmgren@tre.se>
2024-04-04 15:10:58 +0200

    test

---
+++ src/main/java/com/example/test20240404/Test20240404Application.java
@@ -0,0 +1,13 @@
+package com.example.test20240404;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test20240404Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test20240404Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test20240404/Test20240404ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test20240404;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test20240404ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
