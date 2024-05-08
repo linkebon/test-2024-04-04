3057fbfb49f60ded98fe4692cfba7288ae979ffa
xstahol <staffan.holmgren@tre.se>
2024-05-08 19:36:01 +0200

    c

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -7,11 +7,13 @@ import java.util.stream.Stream;
 @SpringBootApplication
 public class TestApplication {
     public static void main(String[] args) {
-        Stream.of("1", "2", "3", "4", "5", "6", "7", "8")
+        System.out.println(Stream.of("1", "2", "3", "4", "5", "6", "7", "8")
               .map(Integer::parseInt)
               .map(i -> i + 1)
               //.filter(i -> (i & 1) == 0)
-              .filter(i -> i % 2 == 0);
+              .filter(i -> i % 2 == 0)
+              .reduce((a, b) -> a + b).get()
+        );
     }
 
 }
