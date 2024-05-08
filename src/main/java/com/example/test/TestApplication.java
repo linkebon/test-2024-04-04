01a5824f41974cc733d1cab8731652fbc6d91849
xstahol <staffan.holmgren@tre.se>
2024-05-08 19:32:49 +0200

    c

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -2,10 +2,18 @@ package com.example.test;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class TestApplication {
     public static void main(String[] args) {
-        //SpringApplication.run(TestApplication.class, args);
+        final Integer sum = Stream.of("1", "2", "3", "4", "5", "6", "7", "8")
+                                  .map(Integer::parseInt)
+                                  .map(i -> i + 1)
+                                  //.filter(i -> (i & 1) == 0)
+                                  .filter(i -> i % 2 == 0)
+                                  .reduce(0, (a, b) -> a + b);
+        System.out.println(sum);
     }
 
 }
