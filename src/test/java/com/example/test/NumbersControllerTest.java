b4e7efdcd82c3b2cd0a0ab131f106951cece6c46
xstahol <staffan.holmgren@tre.se>
2024-05-08 19:46:05 +0200

    step 2-4

--- src/test/java/com/example/test/NumbersControllerTest.java
+++ src/test/java/com/example/test/NumbersControllerTest.java
@@ -1,24 +1,38 @@
 package com.example.test;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestPropertySource(properties = {"management.port=0"})
 class NumbersControllerTest {
 
-        @LocalServerPort
-        private int port;
+    @LocalServerPort
+    private int port;
 
-        @Autowired
-        private RestTemplateBuilder restTemplateBuilder;
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
 
-        @Test
-        public void test() {
-            // write test here that uses /numbers/sum and checks the result
-        }
+    @Test
+    public void test() {
+        final RestTemplate restTemplate = restTemplateBuilder.build();
+
+        final UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString("http://localhost:" + port + "/numbers/sum")
+                                                                              .queryParam("numbers", "1,2,3,4,5");
+        final String body = restTemplate.getForEntity(uriComponentsBuilder.toUriString(),
+                                                      String.class,
+                                                      Map.of("numbers", "1,2,3,4,5"))
+                                        .getBody();
+        Assertions.assertEquals("{\"sum\":12}", body);
+    }
 
 }
